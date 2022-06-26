91865a4c0cbe3624e976c9620df29c69f8c83c3b
dhomo <arhon.one@gmail.com>
2022-06-26 12:36:23 +0500

    теперь все реально выполняется внутри одной транзакции

--- src/main/java/mega/market/server/service/ShopUnitService.java
+++ src/main/java/mega/market/server/service/ShopUnitService.java
@@ -31,72 +31,66 @@ public class ShopUnitService {
     }
 
     @Transactional
-    public boolean imports(ShopUnitImportRequest shopUnitImportRequest) {
-        try {
-            OffsetDateTime updateDate = shopUnitImportRequest.getUpdateDate();
-            // сначала пачкой добавляем и обновляем все что получили, не вникая в структуру
-            // иначе можно нарваться на добавление чилдрена раньше парента
-            Map<UUID, ShopUnit> shopUnitSet = new HashMap<UUID, ShopUnit>();
-            for (ShopUnitImport shopUnitImport : shopUnitImportRequest.getItems()) {
-                ShopUnit shopUnit = shopUnitRepository.findById(shopUnitImport.getId()).orElse(new ShopUnit());
-                if (shopUnitImport.getType() == ShopUnitType.CATEGORY && shopUnitImport.getPrice() != null) {
-                    throw new Exception("у категорий поле price должно содержать null");
-                }
-                if (shopUnitImport.getType() == ShopUnitType.OFFER && (shopUnitImport.getPrice() == null || shopUnitImport.getPrice() < 0)) {
-                    throw new Exception("цена товара не может быть null и должна быть больше либо равна нулю.");
-                }
-                if (shopUnit.getType() != null && shopUnit.getType() != shopUnitImport.getType()) {
-                    throw new Exception("Изменение типа элемента с товара на категорию или с категории на товар не допускается.");
-                }
-                shopUnit.setPrice(shopUnitImport.getPrice());
-                shopUnit.setId(shopUnitImport.getId());
-                shopUnit.setType(shopUnitImport.getType());
-                shopUnit.setName(shopUnitImport.getName());
-                shopUnit.setDate(updateDate);
-
-                shopUnitSet.put(shopUnitImport.getId(), shopUnit);
+    public void imports(ShopUnitImportRequest shopUnitImportRequest) throws Exception {
+        OffsetDateTime updateDate = shopUnitImportRequest.getUpdateDate();
+        // сначала пачкой добавляем и обновляем все что получили, не вникая в структуру
+        // иначе можно нарваться на добавление чилдрена раньше парента
+        Map<UUID, ShopUnit> shopUnitSet = new HashMap<UUID, ShopUnit>();
+        for (ShopUnitImport shopUnitImport : shopUnitImportRequest.getItems()) {
+            ShopUnit shopUnit = shopUnitRepository.findById(shopUnitImport.getId()).orElse(new ShopUnit());
+            if (shopUnitImport.getType() == ShopUnitType.CATEGORY && shopUnitImport.getPrice() != null) {
+                throw new Exception("у категорий поле price должно содержать null");
             }
-            shopUnitRepository.saveAllAndFlush(shopUnitSet.values());
-
-
-            // а сейчас начинаем разираться со cтруктурой
-            Set<ShopUnit> shopRootCategorySet = new HashSet<ShopUnit>();
-            for (ShopUnitImport shopUnitImport : shopUnitImportRequest.getItems()) {
-                ShopUnit shopUnit = shopUnitSet.get(shopUnitImport.getId());
-
-                // обрабатываем изменение парента
-                ShopUnit oldParent = shopUnit.getParent();
-                ShopUnit newParent = null;
-                if (shopUnitImport.getParentId() != null) {
-                    newParent = shopUnitRepository.findById(shopUnitImport.getParentId()).orElseThrow();
-                }
-
-                if (isParentEquals(oldParent, newParent)) {
-                    shopRootCategorySet.add(recalcPrice(oldParent, updateDate));
-                } else {
-                    shopUnit.setParent(newParent);
-                    if (newParent != null) {
-                        if (newParent.getType() == ShopUnitType.OFFER){
-                            throw new RuntimeException("родителем товара или категории может быть только категория");
-                        }
-                        //TODO проверить, возможно это лишнее
-                        newParent.addChildrenItem(shopUnit);
-                    }
-                    if (oldParent != null) {
-                        oldParent.getChildren().remove(shopUnit);
-                    }
-                    shopRootCategorySet.add(recalcPrice(newParent, updateDate));
-                    shopRootCategorySet.add(recalcPrice(oldParent, updateDate));
-                }
+            if (shopUnitImport.getType() == ShopUnitType.OFFER && (shopUnitImport.getPrice() == null || shopUnitImport.getPrice() < 0)) {
+                throw new Exception("цена товара не может быть null и должна быть больше либо равна нулю.");
             }
-            // может попасть null , убираем его
-            shopRootCategorySet.remove(null);
-            shopUnitRepository.saveAllAndFlush(shopRootCategorySet);
+            if (shopUnit.getType() != null && shopUnit.getType() != shopUnitImport.getType()) {
+                throw new Exception("Изменение типа элемента с товара на категорию или с категории на товар не допускается.");
+            }
+            shopUnit.setPrice(shopUnitImport.getPrice());
+            shopUnit.setId(shopUnitImport.getId());
+            shopUnit.setType(shopUnitImport.getType());
+            shopUnit.setName(shopUnitImport.getName());
+            shopUnit.setDate(updateDate);
 
-        } catch (Exception e) {
-            return false;
+            shopUnitSet.put(shopUnitImport.getId(), shopUnit);
         }
-        return true;
+        shopUnitRepository.saveAll(shopUnitSet.values());
+
+
+        // а сейчас начинаем разираться со cтруктурой
+        Set<ShopUnit> shopRootCategorySet = new HashSet<ShopUnit>();
+        for (ShopUnitImport shopUnitImport : shopUnitImportRequest.getItems()) {
+            ShopUnit shopUnit = shopUnitSet.get(shopUnitImport.getId());
+
+            // обрабатываем изменение парента
+            ShopUnit oldParent = shopUnit.getParent();
+            ShopUnit newParent = null;
+            if (shopUnitImport.getParentId() != null) {
+                newParent = shopUnitRepository.findById(shopUnitImport.getParentId()).orElseThrow();
+            }
+
+            if (isParentEquals(oldParent, newParent)) {
+                shopRootCategorySet.add(recalcPrice(oldParent, updateDate));
+            } else {
+                shopUnit.setParent(newParent);
+                if (newParent != null) {
+                    if (newParent.getType() == ShopUnitType.OFFER) {
+                        throw new RuntimeException("родителем товара или категории может быть только категория");
+                    }
+                    //TODO проверить, возможно это лишнее
+                    newParent.addChildrenItem(shopUnit);
+                }
+                if (oldParent != null) {
+                    oldParent.getChildren().remove(shopUnit);
+                }
+                shopRootCategorySet.add(recalcPrice(newParent, updateDate));
+                shopRootCategorySet.add(recalcPrice(oldParent, updateDate));
+            }
+        }
+        // может попасть null , убираем его
+        shopRootCategorySet.remove(null);
+        shopUnitRepository.saveAllAndFlush(shopRootCategorySet);
     }
 
     // взвращает самого верхнего в иерархии
