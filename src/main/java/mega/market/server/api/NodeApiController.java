f3f974bf0acae7d7e07791d4ad07eaae69fb6915
dhomo <arhon.one@gmail.com>
2022-09-08 20:55:01 +0500

    /node/{id}/statistic отрефакторил

--- src/main/java/mega/market/server/api/NodeApiController.java
+++ src/main/java/mega/market/server/api/NodeApiController.java
@@ -7,36 +7,50 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import javax.validation.constraints.*;
-import java.io.IOException;
 import java.time.Instant;
+import java.util.Set;
 import java.util.UUID;
+
+import static java.util.stream.Collectors.toSet;
 
 @RestController
 @RequiredArgsConstructor
-public class NodeApiController implements NodeApi {
+public class NodeApiController {
 
     private static final Logger log = LoggerFactory.getLogger(NodeApiController.class);
     private final ObjectMapper objectMapper;
     private final HttpServletRequest request;
+    private final ShopUnitService shopUnitService;
 
 
+    @GetMapping(path = "/node/{id}/statistic",
+            produces = {"application/json"})
     public ResponseEntity<ShopUnitStatisticResponse> nodeIdStatisticGet(@PathVariable("id") UUID id,
                                                                         @Valid @RequestParam(value = "dateStart", required = false) Instant dateStart,
                                                                         @Valid @RequestParam(value = "dateEnd", required = false) Instant dateEnd) {
-        try {
-            return new ResponseEntity<>(objectMapper.readValue("{\n  \"items\" : [ {\n    \"id\" : \"3fa85f64-5717-4562-b3fc-2c963f66a444\",\n    \"name\" : \"Оффер\",\n    \"date\" : \"2022-05-28T21:12:01.000Z\",\n    \"parentId\" : \"3fa85f64-5717-4562-b3fc-2c963f66a333\",\n    \"price\" : 234,\n    \"type\" : \"OFFER\"\n  }, {\n    \"id\" : \"3fa85f64-5717-4562-b3fc-2c963f66a444\",\n    \"name\" : \"Оффер\",\n    \"date\" : \"2022-05-28T21:12:01.000Z\",\n    \"parentId\" : \"3fa85f64-5717-4562-b3fc-2c963f66a333\",\n    \"price\" : 234,\n    \"type\" : \"OFFER\"\n  } ]\n}", ShopUnitStatisticResponse.class), HttpStatus.NOT_IMPLEMENTED);
 
-        } catch (IOException e) {
-            log.error("Couldn't serialize response for content type application/json", e);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        Set<ShopUnit> items = shopUnitService.nodeIdStatisticGet(id, dateStart, dateEnd);
+
+        ShopUnitStatisticResponse response = new ShopUnitStatisticResponse();
+        response.setItems(items.stream().map(s -> ShopUnitStatisticUnit.builder()
+                        .id(s.getId())
+                        .name(s.getName())
+                        .parentId(s.getParentId())
+                        .type(s.getType())
+                        .price(s.getPrice())
+                        .date(s.getDate())
+                        .build())
+                .collect(toSet()));
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
